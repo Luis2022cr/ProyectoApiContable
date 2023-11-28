fee734bf9438d5c5c328a85c389de37e5470e6ec
estherargueta <arguetaesther03@gmail.com>
2023-11-28 10:09:32 -0600

    Detalles Dtos

--- ProyectoApiContable/ProyectoApiContable/Dtos/Catalogos/CatalogoCreateDto.cs
+++ ProyectoApiContable/ProyectoApiContable/Dtos/Catalogos/CatalogoCreateDto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoApiContable.Dtos.Catalogos
 {
@@ -9,5 +10,23 @@ namespace ProyectoApiContable.Dtos.Catalogos
         [Required(ErrorMessage = "El {0} es requerido")]
 
         public string Name { get; set; }
+
+        [Display(Name = "Usuario")]
+        [StringLength(70, ErrorMessage = "El {0} requiere {1} caracteres")]
+        [Required(ErrorMessage = "El {0} es requerido")]
+
+        public string User { get; set; }
+
+        [Display(Name = "Descripcion")]
+        [StringLength(70, ErrorMessage = "El {0} requiere {1} caracteres")]
+        [Required(ErrorMessage = "El {0} es requerido")]
+
+        public string Description { get; set; }
+
+        [Display(Name = "Fecha")]
+        [StringLength(70, ErrorMessage = "El {0} requiere {1} caracteres")]
+        [Required(ErrorMessage = "El {0} es requerido")]
+
+        public DateTime Fecha { get; set; }
     }
 }
--- ProyectoApiContable/ProyectoApiContable/Dtos/Catalogos/CatalogoGetByIdDto.cs
+++ ProyectoApiContable/ProyectoApiContable/Dtos/Catalogos/CatalogoGetByIdDto.cs
@@ -1,6 +1,14 @@
-﻿namespace ProyectoApiContable.Dtos.Catalogos
+﻿using ProyectoApiContable.Entities;
+
+namespace ProyectoApiContable.Dtos.Catalogos
 {
     public class CatalogoGetByIdDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Usuario { get; set; }
+        public string Description { get; set; }
+        public DateTime Fecha { get; set; }
+
     }
 }
